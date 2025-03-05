ff102c05a809bcd6b7a274cbe60eedb177cd652a
winsakshi10 <winsakshi10@gmail.com>
2025-03-05 12:12:34 +0000

    applied dark theme

--- nvidia-benchmark/app/page.tsx
+++ nvidia-benchmark/app/page.tsx
@@ -1,11 +1,29 @@
+"use client";
 import { LoginForm } from "@/components/login-form";
+import { Card, CardContent } from "@/components/ui/card";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Page() {
+
+
+
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm font-mono ">
         <LoginForm />
+        <Card>
+          <CardContent>
+            <Typewriter words={["This is a sample text that appears like typing."] }
+            loop = {1}
+            typeSpeed={50}
+            deleteSpeed={0}
+            cursorStyle= {"|"}
+            />
+           
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
-}
+} 
